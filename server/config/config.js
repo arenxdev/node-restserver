7afa669675a528d8a3c0380c274de0d8faa6d2a1
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2020-02-20 22:51:13 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,9 +7,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Base de datos
 
-const urlDB =
-  process.env.NODE_ENV === 'dev'
-    ? 'mongodb://localhost:27017/cafeudemy'
-    : 'mongodb+srv://aarondev:aarondev@mongocloud-8zchy.mongodb.net/cafeudemy';
+const urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafeudemy' : process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
